25c6947922f7ccfd7737ad5bd23d517d823b3011
Akshat-Gup <69629823+Akshat-Gup@users.noreply.github.com>
2021-01-27 08:23:57 +0800

    Fixed Formatting issues

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 let ball = document.querySelector(".eightballback");
 let text = document.querySelector("h3");
 let responses = ['As I see it, yes.', "Ask again later."," Better not tell you now.","Cannot predict now.","Don’t count on it.","It is certain."," Yes.","No.","Never.","No Way!", "Absolutely not!"]
-document.body.addEventListener("click", () => {
+ball.addEventListener("click", () => {
 	text.classList.add("rotate");
 	setTimeout(() => {
 		text.classList.remove("rotate");
 		let index = Math.floor(Math.random() * responses.length);
 		console.log(index);
-		text.style.fontSize = '3em';
+		text.style.fontSize = '2em';
+		text.style.padding = '30% 0 0 0';
 		text.textContent = responses[index];
 	}, 1000);
 });
